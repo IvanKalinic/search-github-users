6715563e3ab5b5730bae9502f8c915ae067c19dc
Ivan Kalinić <ik002312821@fer.hr>
2021-05-05 20:18:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        {/* <Navbar/> */}
+        <Switch>
+          <PrivateRoute path="/" exact={true}>
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path="/login" exact={true}>
+            <Login></Login>
+          </Route>
+          <Route path="*">
+            <Error> </Error>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
